c9243b92104d537c1076f2832297eb9dffeea584
EyifaDavid <102861958+EyifaDavid@users.noreply.github.com>
2025-06-13 12:53:38 +0000

    test

--- frontend/fashion_e-commerce/src/redux/slices/apiSlice.js
+++ frontend/fashion_e-commerce/src/redux/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
 
-const API_URI = import.meta.env.VITE_APP_BASE_URL
+const API_URI = import.meta.env.VITE_APP_BASE_URL;
 
 const baseQuery = fetchBaseQuery({baseUrl: API_URI, credentials:'include'
 })
