1444f28ee66de301435a7faafbaa2d4708e8fd75
EyifaDavid <102861958+EyifaDavid@users.noreply.github.com>
2025-06-15 23:11:33 +0000

    corsoption

--- backend/index.js
+++ backend/index.js
@@ -31,11 +31,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const upload = multer({ dest: 'uploads/' }); // Temporary upload dir
 
-app.use(cors({
-    origin: ["http://localhost:4000","http://localhost:4001","https://mavraudercollections.netlify.app"],
-    methods: ["GET","POST","PUT","DELETE"],
-    credentials:true,
-}));
+// app.use(cors({
+//     origin: ["http://localhost:4000","http://localhost:4001","https://mavraudercollections.netlify.app"],
+//     methods: ["GET","POST","PUT","DELETE"],
+//     credentials:true,
+// }));
 
 const corsOptions = {
   origin: ["https://mavraudercollections.netlify.app","http://localhost:4000"],
